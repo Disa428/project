7ac009980597c82f2dc992b38243bd5f036bdaf4
Den <disa428@yandex.ru>
2021-11-06 22:08:32 +0400

    Reviu koda

--- JS/Practika.js
+++ JS/Practika.js
@@ -2,23 +2,20 @@
 
 const namberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const PersonalMuvieDB = {
+const PersonalMovieDB = {
   count: namberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false
-
 };
 
-const a = prompt("Один из последних просмотренных фидьмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фидьмов?", ""),
-      d = prompt("На сколько оцените его?", "");
-  
 
- PersonalMuvieDB.movies[a] = b;
- PersonalMuvieDB.movies[c] = d;
+//Практика 2 
 
- console.log(PersonalMuvieDB);
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фидьмов?", ""),
+        b = prompt("На сколько оцените его?", "");
 
+        PersonalMovieDB.movies[a] = b;    
+}
